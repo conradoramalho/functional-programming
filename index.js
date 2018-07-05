b950217d6b2f067b7358a05a49b23bbd9c72e4a4
conradoramalho <conrado.ramalho94@gmail.com>
2018-07-05 20:17:44 -0300

    add more examples

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ x2(y);
 x1(y);
 
 console.log(x1(x2(y)).val); // 5
+
+const add = a => b => a + b;
+
+const first = add(10);
+
+console.log(first(20));
+
+//Side Effects
+let meetup = { name: "JS", isActive: true, members: 49 };
+
+const scheduleMeetup = (date, place) => {
+  meetup.date = date;
+  meetup.place = place;
+  if (meetup.members < 50) meetup.isActive = false;
+};
+const publishMeetup = () => {
+  if (meetup.isActive) {
+    meetup.publish = true;
+  }
+};
+scheduleMeetup("today", "Bnagalore");
+publishMeetup();
+console.log(meetup);
